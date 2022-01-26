4ef35adcd0f4b270885c862be2da8f22c1868dcb
Christoph Rohrer <christoph@rohrers.de>
2022-01-27 00:30:28 +0100

    Initial Commit

---
+++ BubbleChart/BubbleChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleChartApp.swift
+//  BubbleChart
+//
+//  Created by Christoph Rohrer on 26.01.22.
+//
+
+import SwiftUI
+
+@main
+struct BubbleChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
